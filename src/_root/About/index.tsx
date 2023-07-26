bb4d070d2538dbae8873b0f7d1f036a6d7bdd44f
Kentbot <kmiller175@gmail.com>
2023-07-25 21:50:08 -0500

    Add About info

--- src/_root/About/index.tsx
+++ src/_root/About/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import { Typography } from '@mui/material'
+
 export default function About() {
   return (
     <>
-      About the site here
+      <Typography variant="h4">
+        About the site
+      </Typography>
+      <Typography variant="body1" sx={{ pt: 3 }}>
+        My goal is to release one new "thing" every week. For this first week the "thing"
+        is the website itself. I plan on having a responsive design, basic layout with navigation,
+        and light/dark theme togglable. I will use Material UI (MUI) and create-react-app, though
+        in the future I may use other frameworks or libraries like Next.js/Nuxt.js, Vue, Svelte,
+        Angular, etc.
+        <br/>
+        <br/>
+        As I come up with more ideas and organize them, I'll populate a list here (or somewhere else on the site, we'll see!)
+      </Typography>
     </>
   )
 }
