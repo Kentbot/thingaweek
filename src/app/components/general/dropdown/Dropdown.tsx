e9a48e4edfaa8aa607ff7b6648e03809c9171349
Kent Miller <kmiller175@gmail.com>
2024-03-04 14:12:32 -0600

    Attempt to get dropdown blur working, will come back later

--- src/app/components/general/dropdown/Dropdown.tsx
+++ src/app/components/general/dropdown/Dropdown.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import './styles.scss'
 
 type Option = {
   value: string
   display: string
+  group?: string
 }
 
 type Props = {
   initialOption?: Option
   options: Option[]
-  onSelect: (value: string) => void
+  onSelect: (value: Option) => void
 }
 
 // TODO: Add option groups, work on selection/focus
@@ -21,39 +22,66 @@ export function Dropdown({
 }: Props) {
   const [active, setActive] = useState(false)
   const [selectedOption, setSelectedOption] = useState(initialOption)
+  const optionRefs = useRef<(HTMLInputElement | null)[]>([])
+
+  useEffect(() => {
+    optionRefs.current.slice(0, options.length)
+  }, [options])
+
+  useEffect(() => {
+    if (active) {
+      const currentOption =
+        optionRefs.current.find(optRef => selectedOption ? optRef?.value === selectedOption.value : false)
+
+      if (currentOption) {
+        currentOption.focus()
+      }
+    }
+  }, [active, selectedOption])
 
   const handleDropdownClick = () => {
     setActive(!active)
   }
 
   const handleOptionClick = (event: React.MouseEvent<HTMLInputElement>) => {
-    console.log('click', event)
-    setActive(false)
+    const wasMouseClick = event.clientX !== 0 && event.clientY !== 0
+    if (wasMouseClick) {
+      setActive(false)
+    }
   }
 
   const handleKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    console.log('key', event.key)
     if (event.key === 'Enter') {
       setActive(false)
     }
   }
 
   const handleChange = (option: Option) => {
-    console.log('change')
-    onSelect(option.value)
+    onSelect(option)
+    setSelectedOption(option)
+  }
+
+  const handleBlur = (event: React.FocusEvent) => {
+    // TODO: Come up with a good way to handle the user clicking outside of the dropdown
+    // if (!event.currentTarget.contains(event.relatedTarget)) {
+    //   setActive(false)
+    // }
   }
 
   return (
-    <div className={`custom-select ${active ? 'active' : ''}`}>
+    <div className={`custom-select ${active ? 'active' : ''}`} onBlur={handleBlur}>
       <button className="select-button" onClick={handleDropdownClick}>
         <span className="selected-value">{selectedOption?.display ?? '-'}</span>
         <span className="arrow"></span>
       </button>
-      <ul className="select-dropdown" onFocus={() => console.log('focus')} onBlur={() => console.log('blur')}>
+      <ul className="select-dropdown" >
         {
-          options.map(opt => (
-            <li key={opt.value}>
+          options.map((opt, i) => (
+            <li
+              key={opt.value}
+              >
               <input
+                ref={el => optionRefs.current[i] = el}
                 type="radio"
                 id={opt.value}
                 name="dropdown-values"
