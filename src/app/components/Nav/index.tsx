0d1355b054f54093c731f7f593ff7889f4d02bde
Kent Miller <kmiller175@gmail.com>
2024-03-03 22:41:26 -0600

    Add nav spacer so I don't have to keep adding top-padding/margin to stuff

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -11,33 +11,36 @@ export function Nav() {
   const pathname = usePathname()
  
   return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <Link
-            className={`link ${pathname === '/' ? 'active' : ''}`}
-            href="/"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={`link ${pathname.startsWith('/budget') ? 'active' : ''}`}
-            href="/budget"
-          >
-            Budget
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={`link ${pathname === '/about' ? 'active' : ''}`}
-            href="/about"
-          >
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link
+              className={`link ${pathname === '/' ? 'active' : ''}`}
+              href="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${pathname.startsWith('/budget') ? 'active' : ''}`}
+              href="/budget"
+            >
+              Budget
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${pathname === '/about' ? 'active' : ''}`}
+              href="/about"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="nav-spacer"></div>
+    </>
   )
 }
