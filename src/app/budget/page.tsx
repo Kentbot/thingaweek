ec27e8d8b649f096143c7060a7c98aa842923052
Kent Miller <kmiller175@gmail.com>
2024-03-04 21:25:56 -0600

    Dropdown select looks nice and works fairly well now

--- src/app/budget/page.tsx
+++ src/app/budget/page.tsx
@@ -13,22 +13,6 @@ export default function Budget() {
         <FontAwesomeIcon icon={faScrewdriverWrench} size={'3x'} />
         This page requires some love and attention
       </div>
-      <Dropdown
-        onSelect={(v) => console.log('selected', v)}
-        options={
-          [
-            { value: 'value 1', display: 'Value #1' },
-            { value: 'value 2', display: 'Value #2' },
-            { value: 'value 3', display: 'Value #3' },
-            { value: 'value 4', display: 'Value #4' },
-            { value: 'value 5', display: 'Value #5' },
-            { value: 'value 6', display: 'Value #6' },
-            { value: 'value 7', display: 'Value #7' },
-            { value: 'value 8', display: 'Value #8' },
-            { value: 'value 9', display: 'Value #9' },
-          ]
-        }
-      />
     </div>
   )
 }
--- src/app/components/general/dropdown/Dropdown.tsx
+++ src/app/components/general/dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { forwardRef, useEffect, useRef, useState } from 'react'
 
 import './styles.scss'
 
@@ -6,6 +6,10 @@ type Option = {
   value: string
   display: string
   group?: string
+}
+
+type GroupedOptions = {
+  [key: string]: Option[]
 }
 
 type Props = {
@@ -21,8 +25,35 @@ export function Dropdown({
   onSelect
 }: Props) {
   const [active, setActive] = useState(false)
+  // The currently selected option, but can be reverted by the user if they hit escape
   const [selectedOption, setSelectedOption] = useState(initialOption)
+  // The option the user has actually confirmed by clicking/hitting 'enter'
+  const [confirmedOption, setConfirmedOption] = useState(initialOption)
+  
+  const [groupedOptions, setGroupedOptions] = useState<GroupedOptions>({})
+  const [ungroupedOptions, setUngroupedOptions] = useState<Option[]>([])
+
   const optionRefs = useRef<(HTMLInputElement | null)[]>([])
+  
+  useEffect(() => {
+    const newGroupedOptions: GroupedOptions = {}
+    const newUngroupedOptions: Option[] = []
+
+    options.forEach(opt => {
+      if (opt.group) {
+        if (!newGroupedOptions[opt.group]) {
+          newGroupedOptions[opt.group] = []
+        }
+        newGroupedOptions[opt.group].push(opt)
+      } else {
+        newUngroupedOptions.push(opt)
+      }
+    })
+
+    setGroupedOptions(newGroupedOptions)
+    setUngroupedOptions(newUngroupedOptions)
+
+  }, [options])
 
   useEffect(() => {
     optionRefs.current.slice(0, options.length)
@@ -31,33 +62,47 @@ export function Dropdown({
   useEffect(() => {
     if (active) {
       const currentOption =
-        optionRefs.current.find(optRef => selectedOption ? optRef?.value === selectedOption.value : false)
+        optionRefs.current.find(optRef => confirmedOption ? optRef?.value === confirmedOption.value : false)
 
       if (currentOption) {
         currentOption.focus()
       }
     }
-  }, [active, selectedOption])
+  }, [active, confirmedOption])
 
   const handleDropdownClick = () => {
     setActive(!active)
   }
 
-  const handleOptionClick = (event: React.MouseEvent<HTMLInputElement>) => {
+  const handleOptionClick = (event: React.MouseEvent<HTMLInputElement>, option: Option) => {
+    console.log('click')
     const wasMouseClick = event.clientX !== 0 && event.clientY !== 0
     if (wasMouseClick) {
+      // This needs the option directly because it fires before the onChange event, which means
+      // it would be one step behind the "current" selected option
+      handleConfirmOption(option || { display: 'Error', value: 'Error' })
       setActive(false)
     }
   }
 
   const handleKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      setActive(false)
+    switch (event.key) {
+      case 'Enter': 
+        setActive(false)
+        handleConfirmOption(selectedOption || { display: 'Error', value: 'Error' })
+        break
+      case 'Escape':
+        setActive(false)
+        break
     }
   }
 
-  const handleChange = (option: Option) => {
+  const handleConfirmOption = (option: Option) => {
+    setConfirmedOption(option)
     onSelect(option)
+  }
+
+  const handleChange = (option: Option) => {
     setSelectedOption(option)
   }
 
@@ -71,29 +116,76 @@ export function Dropdown({
   return (
     <div className={`custom-select ${active ? 'active' : ''}`} onBlur={handleBlur}>
       <button className="select-button" onClick={handleDropdownClick}>
-        <span className="selected-value">{selectedOption?.display ?? '-'}</span>
+        <span className="selected-value">{confirmedOption?.display ?? '-'}</span>
         <span className="arrow"></span>
       </button>
       <ul className="select-dropdown" >
         {
-          options.map((opt, i) => (
-            <li
-              key={opt.value}
-              >
-              <input
+          ungroupedOptions.map((opt, i) => (
+            <React.Fragment key={opt.value}>
+              <DropdownOption
                 ref={el => optionRefs.current[i] = el}
-                type="radio"
-                id={opt.value}
-                name="dropdown-values"
-                value={opt.value}
-                onKeyDown={handleKeydown}
-                onClick={handleOptionClick}
-                onChange={() => handleChange(opt)}/>
-              <label htmlFor={opt.value}>{opt.display}</label>
-            </li>
+                option={opt}
+                onKeydown={handleKeydown}
+                onOptionClick={handleOptionClick}
+                onChange={() => handleChange(opt)}
+              />
+            </React.Fragment>
           ))
+        }
+        {
+          Object.keys(groupedOptions).map((group) => {
+            const groupOpts = groupedOptions[group]
+
+            return (
+              <React.Fragment key={group}>
+                <div className="group-divider">
+                  <div className="label">{groupOpts[0].group}</div><hr/>
+                </div>
+                <div className="option-group">
+                  {
+                    groupOpts.map((opt, i) => (
+                      <React.Fragment key={opt.value}>
+                        <DropdownOption
+                          ref={el => optionRefs.current[i] = el}
+                          option={opt}
+                          onKeydown={handleKeydown}
+                          onOptionClick={handleOptionClick}
+                          onChange={() => handleChange(opt)}
+                        />
+                      </React.Fragment>))
+                  }
+                </div>
+              </React.Fragment>
+            )
+          })
         }
       </ul>
     </div>
   )
 }
+
+type OptionProps = {
+  option: Option
+  onKeydown: (event: React.KeyboardEvent<HTMLInputElement>) => void
+  onOptionClick: (event: React.MouseEvent<HTMLInputElement>, option: Option) => void
+  onChange: (option: Option) => void
+}
+
+const DropdownOption = forwardRef<HTMLInputElement, OptionProps>(
+  function DropdownOption({ option, onKeydown, onOptionClick, onChange }, ref) {
+  return (
+    <li>
+      <input
+        ref={ref}
+        type="radio"
+        id={option.value}
+        name="dropdown-values"
+        value={option.value}
+        onKeyDown={onKeydown}
+        onClick={(event) => onOptionClick(event, option)}
+        onChange={() => onChange(option)}/>
+      <label htmlFor={option.value}>{option.display}</label>
+    </li>
+  )
+})
