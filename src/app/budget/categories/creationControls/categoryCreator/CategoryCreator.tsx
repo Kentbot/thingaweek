e1a03ec20802868c181af33e45057892060396c8
Kent Miller <kmiller175@gmail.com>
2024-03-12 21:05:08 -0500

    Starting work on input validation

--- src/app/budget/categories/creationControls/categoryCreator/CategoryCreator.tsx
+++ src/app/budget/categories/creationControls/categoryCreator/CategoryCreator.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 import currency from 'currency.js'
@@ -16,14 +16,35 @@ export function CategoryCreator() {
   const dispatch = useDispatch<AppDispatch>()
   const currentMonth = useSelector((state: RootState) => state.budgetMonth)
 
+  const nameRef = useRef<HTMLInputElement>(null)
+
   const [categoryName, setCategoryName] = useState('')
+  const [nameIsValid, setNameIsValid] = useState(true)
   const [budgetAmt, setBudgetAmt] = useState('')
+
+  const handleNameChange = (value: string) => {
+    if (!nameIsValid) {
+      setNameIsValid(validateName(value))
+    }
+    setCategoryName(value)
+  }
+
+  const validateName = (name: string) => {
+    return name.length > 0
+  }
 
   const handleBudgetChange = (value: string) => {
     setBudgetAmt(value)
   }
 
   const handleCategoryCreate = () => {
+    const isValidName = validateName(categoryName)
+    setNameIsValid(isValidName)
+
+    if (!isValidName) {
+      return
+    }
+
     // Note that this only works for creating a new category from scratch.
     // It will not work for copying from a previous month -- the EOM balance
     // and previous month need set.
@@ -31,12 +52,11 @@ export function CategoryCreator() {
       id: nanoid(),
       name: categoryName,
       additionalIncome: currency(0).toString(),
-      balanceForward: currency(0).toString(),
       budgetedAmount: currency(budgetAmt).toString(),
       endOfMonthAdjust: currency(0).toString(),
-      endOfMonthBalance: currency(budgetAmt).toString(),
       transactionIds: [],
-      budgetMonth: currentMonth
+      budgetMonth: currentMonth,
+      linkedMonths: {}
     }))
 
     setCategoryName('')
@@ -48,12 +68,13 @@ export function CategoryCreator() {
       <div className="label">Create New Category <Tooltip onClick={() => alert('TODO Category')}/></div>
       {/* TODO: Add form validation logic so that empty group/category names are impossible */}
       <input
+        ref={nameRef}
         id='category-name-input'
-        className='category-input'
+        className={`category-input ${!nameIsValid ? 'invalid' : ''}`}
         type='text'
         placeholder='Category name'
         value={categoryName}
-        onChange={(v) => setCategoryName(v.target.value)}
+        onChange={(v) => handleNameChange(v.target.value)}
       />
       <NumericInput
         id='budget-amt-input'
