94d316275fd5e2413ae65234d38f52daa96bb572
Kent Miller <kmiller175@gmail.com>
2024-03-14 22:11:54 -0500

    Fix typo in style import

--- src/app/budget/categories/creationControls/CreationControls.tsx
+++ src/app/budget/categories/creationControls/CreationControls.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil } from '@fortawesome/free-solid-svg-icons'
+
 import { Modal } from '@components/general/modal/Modal'
 
 import { CategoryCreator } from './categoryCreator/CategoryCreator'
 import { GroupCreator } from './groupCreator/GroupCreator'
 import { MonthCarryover } from './monthCarryover/MonthCarryover'
-
-import './creationCOntrol.styles.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPencil } from '@fortawesome/free-solid-svg-icons'
 import { IncomeCreator } from './incomeCreator/IncomeCreator'
 
-export function CreationControls() {
+import './creationControl.styles.scss'
 
+export function CreationControls() {
   return (
     <div className="creation-controls">
       <Modal 
