b537d02e855e0b10fc91ae1ea57ce6001f508a1f
Kent Miller <kmiller175@gmail.com>
2024-04-15 10:45:01 -0500

    Test working, but incomplete

--- src/app/budget/categories/categories.spec.tsx
+++ src/app/budget/categories/categories.spec.tsx
@@ -10,15 +10,17 @@ describe('Budget Category Page', () => {
   describe('category carryover', () => {
     describe('income categories', () => {
       it('carries over from month to month', async () => {
-        const { user, findByRole } = render(<CategoriesPage />, testStates.default)
+        const { user, findByRole, findByPlaceholderText } = render(<CategoriesPage />, testStates.default)
 
         await user.click(await findByRole('button', { name: /category management/i }))
 
-        await user.click(await findByRole('textbox', { description: /income category name/i } ))
+        await user.click(await findByPlaceholderText(/income category name/i))
 
         await user.keyboard('test income')
 
         await user.click(await findByRole('button', { name: /create income/i }))
+
+        await user.keyboard('[Escape]')
       })
     })
   })
