9f0a1606a86a073e79ce6c1fc6040714497c564e
Kent Miller <kmiller175@gmail.com>
2024-04-18 10:30:58 -0500

    First component test working

--- src/app/budget/categories/categories.spec.tsx
+++ src/app/budget/categories/categories.spec.tsx
@@ -9,18 +9,31 @@ describe('Budget Category Page', () => {
 
   describe('category carryover', () => {
     describe('income categories', () => {
-      it('carries over from month to month', async () => {
-        const { user, findByRole, findByPlaceholderText } = render(<CategoriesPage />, testStates.default)
+      it('is created correctly', async () => {
+        const {
+          user,
+          findByRole,
+          findByPlaceholderText,
+          findByText,
+          findAllByText,
+        } = render(<CategoriesPage />, testStates.default)
 
         await user.click(await findByRole('button', { name: /category management/i }))
 
         await user.click(await findByPlaceholderText(/income category name/i))
 
         await user.keyboard('test income')
+        await user.tab()
+        await user.keyboard('150.15')
 
         await user.click(await findByRole('button', { name: /create income/i }))
 
         await user.keyboard('[Escape]')
+
+        const incomeTitle = await findByText('test income')
+        expect(incomeTitle.parentElement?.className).toBe('income-grid')
+        const expectedIncome = await findAllByText('150.15')
+        expect(expectedIncome.some(el => el.parentElement?.className === 'income-grid')).toBeTruthy()
       })
     })
   })
