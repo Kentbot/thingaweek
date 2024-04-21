e2d17b3589b33b9b02b6417f6cc3d34882cb91dc
Kent Miller <kmiller175@gmail.com>
2024-04-21 09:26:35 -0500

    Remove redundant css import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import { Nav } from '@/components/MainNav'
 // From: https://stackoverflow.com/questions/56334381
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import '@fortawesome/fontawesome-svg-core/styles.css'
 // Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core';
+import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false; /* eslint-disable import/first */
 
 import './globals.scss'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import './globals.scss'
-
 export default function Home() {
   return (
     <>
