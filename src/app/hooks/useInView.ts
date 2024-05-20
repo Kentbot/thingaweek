c66768345235adf1c9022693354a8177c3a02626
Kent Miller <kmiller175@gmail.com>
2024-05-20 18:18:02 -0500

    Fix intersectionobserver SSR issue

--- src/app/hooks/useInView.ts
+++ src/app/hooks/useInView.ts
@@ -1,18 +1,20 @@
-import { useState, useMemo, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function useInView(ref: React.RefObject<HTMLElement>) {
   const [isIntersecting, setIntersecting] = useState(false)
 
-  const observer = useMemo(() => new IntersectionObserver(
-    ([entry]) => setIntersecting(entry.isIntersecting)
-  ), [])
-
+  
   useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting)
+    )
+
     if (ref.current) {
       observer.observe(ref.current)
     }
+
     return () => observer.disconnect()
-  }, [observer, ref])
+  }, [ref])
 
   return isIntersecting
 }
