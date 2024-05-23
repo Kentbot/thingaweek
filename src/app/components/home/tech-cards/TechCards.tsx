0e5158d55aedcf52e7b7f3239a17397c6e612a17
Kent Miller <kmiller175@gmail.com>
2024-05-22 20:23:39 -0500

    Better (but not "good") main page tech icons

--- src/app/components/home/tech-cards/TechCards.tsx
+++ src/app/components/home/tech-cards/TechCards.tsx
@@ -30,13 +30,14 @@ export function TechCards() {
     <div className={styles["tech-cards"]}>
       <Card onClick={() => handleCardClick('angular')} selected={selectedCard === 'angular'}>
         <div className={styles["icon-wrapper"]}>
-          <Link href={"/"} style={{ "display": "flex", "justifyContent": "space-evenly" }}>
-              <img
-                src="https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg"
-                alt="Angular Icon"
-                className={styles["tech-icon"]}
-              />
-          </Link>
+          <img
+            src="https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg"
+            alt="Angular Icon"
+            className={styles["tech-icon"]}
+          />
+        </div>
+        <div className={styles["title"]}>
+          Angular
         </div>
       </Card>
       <Card onClick={() => handleCardClick('react')} selected={selectedCard === 'react'}>
@@ -45,11 +46,61 @@ export function TechCards() {
         </div>
       </Card>
       <Card onClick={() => handleCardClick('vue')} selected={selectedCard === 'vue'}>
-        <Link href={"/"}>
-          <div className={styles["icon-wrapper"]}>
-            <VueIcon />
-          </div>
-        </Link>
+        <div className={styles["icon-wrapper"]}>
+          <VueIcon />
+        </div>
+      </Card>
+      <Card onClick={() => handleCardClick('jsts')} selected={selectedCard === 'jsts'}>
+        <div className={styles["icon-wrapper"]} id={styles["icon-1"]}>
+          <img
+            src="https://www.typescriptlang.org/icons/icon-144x144.png?v=8944a05a8b601855de116c8a56d3b3ae"
+            alt="Typescript Icon"
+            className={styles["tech-icon"]}
+          />
+        </div>
+        <div className={styles["icon-wrapper"]} id={styles["icon-2"]}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/320px-Unofficial_JavaScript_logo_2.svg.png"
+            alt="Javascript Icon"
+            className={styles["tech-icon"]}
+          />
+        </div>
+      </Card>
+      <Card onClick={() => handleCardClick('vite')} selected={selectedCard === 'vite'}>
+        <div className={styles["icon-wrapper"]}>
+          <img
+            src="https://vitejs.dev/logo-with-shadow.png"
+            alt="Vite Icon"
+            className={styles["tech-icon"]}
+          />
+        </div>
+      </Card>
+      <Card onClick={() => handleCardClick('webpack')} selected={selectedCard === 'webpack'}>
+        <div className={styles["icon-wrapper"]}>
+          <img
+            src="https://webpack.js.org/favicon.a3dd58d3142f7566.ico"
+            alt="Webpack Icon"
+            className={styles["tech-icon"]}
+          />
+        </div>
+      </Card>
+      <Card onClick={() => handleCardClick('csharp')} selected={selectedCard === 'csharp'}>
+        <div className={styles["icon-wrapper"]}>
+          <img
+            src="https://learn.microsoft.com/en-us/dotnet/media/logo_csharp.png"
+            alt="C-Sharp Icon"
+            className={styles["tech-icon"]}
+          />
+        </div>
+      </Card>
+      <Card onClick={() => handleCardClick('dotnet')} selected={selectedCard === 'dotnet'}>
+        <div className={styles["icon-wrapper"]}>
+          <img
+            src="https://dotnet.microsoft.com/favicon.ico"
+            alt="Dotnet Icon"
+            className={styles["tech-icon"]}
+          />
+        </div>
       </Card>
     </div>
   )
